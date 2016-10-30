966142bf381993168b05d980555bccc3cd06ca7b
Oeromilk <oero.mccomb@gmail.com>
2016-10-29 20:26:40 -0400

    fixed view listing

--- app/scripts/components/menu_view.jsx
+++ app/scripts/components/menu_view.jsx
@@ -1,38 +1,83 @@
 var React = require('react');
+var Backbone = require('backbone');
 
 var TemplateComponent = require('./template.jsx').TemplateComponent;
+var menuItems = require('../menu_items.js');
+
+var OrderComponent = React.createClass({
+  render: function(){
+    return (
+      <div className="col-md-3">
+        <div>Your Order</div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <td>Subtotal</td>
+              <td>Overall price</td>
+            </tr>
+          </tfoot>
+          <tbody>
+            <tr>
+              <td>Sample Text</td>
+              <td>9.99</td>
+            </tr>
+          </tbody>
+        </table>
+        <button type="button" className="btn btn-primary btn-lg btn-block">Check Out</button>
+      </div>
+    );
+  }
+});
 
 var MenuList = React.createClass({
   render: function(){
+    console.log(this.props.menuItems);
 
+    var menuListItems = this.props.menuItems.map(function(content){
+      console.log(content);
+      return (
+        <tr key={content.id}>
+          <td>
+            <h4>{content.item}</h4>
+            <p>{content.description}</p>
+            <button type="button" className="btn btn-info">{content.price}</button>
+          </td>
+        </tr>
+      );
+    });
     return (
-      <div className="col-md-8 col-md-offset-2">
+      <div className="col-md-9">
         <table className="table">
           <thead>
             <tr>
               <th><h3>Curry</h3></th>
             </tr>
           </thead>
-          <tbody>
-            <tr className="well">
-              <td><h4>Item Name</h4><p>item description</p><button>price here</button></td>
-            </tr>
+          <tbody  className="well">
+            {menuListItems}
           </tbody>
         </table>
       </div>
-    )
+    );
   }
-})
+});
 
 var MenuContainer = React.createClass({
   render: function(){
     return (
       <TemplateComponent>
-        <MenuList />
+        <MenuList menuItems={menuItems}/>
+        <OrderComponent />
       </TemplateComponent>
     );
   }
-})
+});
 
 module.exports = {
   MenuContainer: MenuContainer
--- app/scripts/menu_items.js
+++ app/scripts/menu_items.js
@@ -1,51 +1,44 @@
 
-var data = [
-  {
-  id: 101,
-  item: 'Green Curry',
-  description: 'Zucchini, eggplant, green beans and basil leaves, simmered in a spicy green curry paste with coconut milk.',
-  price: 9.99
-  },
-  {
-  id: 102,
-  item: 'Red Curry',
-  description: 'Bell peppers, bamboo shoots, eggplant and basil leaves. This is simmered in a red curry paste with coconut milk.',
-  price: 9.99
-  },
-  {
-  id: 103,
-  item: 'Yellow Curry',
-  description: 'Mild yellow curry, containing rich flavors of curry powder, coconut milk, potatoes, onions and carrots.',
-  price: 7.99
-  },
-  {
-  id: 104,
-  item: 'Pa-Neng Curry',
-  description: 'Sweet thick paste of pa-neng curry, coconut milk, bell peppers and basil leaves.',
-  price: 11.99
-  },
-  {
-  id: 106,
-  item: 'Massaman Curry',
-  description: 'This is a favorite Thai curry with potatoes, onions, and peanuts in coconut milk.',
-  price: 8.99
-  },
-  {
-  id: 107,
-  item: 'Pineapple Curry',
-  description: 'Diced pineapples, bell peppers, basil leaves, in spicy Thai red curry paste.',
-  price: 10.99
-  },
-  {
-  id: 108,
-  item: 'Jungle Curry',
-  description: 'With choice of meat, bell peppers, green beans, squash, bamboo shoots, basil, mushrooms, baby corn, all cooked with jungle curry paste.',
-  price: 12.99
-  },
-  {
-  id: 109,
-  item: 'Roasted Duck Curry',
-  description: 'Sliced roasted duck with pineapple chunks, tomato, bell pepper and basil in red curry paste and coconut mlk.',
-  price: 13.99
-  },
-]
+var data = [{
+  'id': 101,
+  'item': 'Green Curry',
+  'description': 'Zucchini, eggplant, green beans and basil leaves, simmered in a spicy green curry paste with coconut milk.',
+  'price': 9.99
+  }, {
+  'id': 102,
+  'item': 'Red Curry',
+  'description': 'Bell peppers, bamboo shoots, eggplant and basil leaves. This is simmered in a red curry paste with coconut milk.',
+  'price': 9.99
+  }, {
+  'id': 103,
+  'item': 'Yellow Curry',
+  'description': 'Mild yellow curry, containing rich flavors of curry powder, coconut milk, potatoes, onions and carrots.',
+  'price': 7.99
+  }, {
+  'id': 104,
+  'item': 'Pa-Neng Curry',
+  'description': 'Sweet thick paste of pa-neng curry, coconut milk, bell peppers and basil leaves.',
+  'price': 11.99
+  }, {
+  'id': 106,
+  'item': 'Massaman Curry',
+  'description': 'This is a favorite Thai curry with potatoes, onions, and peanuts in coconut milk.',
+  'price': 8.99
+  }, {
+  'id': 107,
+  'item': 'Pineapple Curry',
+  'description': 'Diced pineapples, bell peppers, basil leaves, in spicy Thai red curry paste.',
+  'price': 10.99
+  }, {
+  'id': 108,
+  'item': 'Jungle Curry',
+  'description': 'With choice of meat, bell peppers, green beans, squash, bamboo shoots, basil, mushrooms, baby corn, all cooked with jungle curry paste.',
+  'price': 12.99
+  }, {
+  'id': 109,
+  'item': 'Roasted Duck Curry',
+  'description': 'Sliced roasted duck with pineapple chunks, tomato, bell pepper and basil in red curry paste and coconut mlk.',
+  'price': 13.99
+}];
+
+module.exports = data;
